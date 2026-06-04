dd251b3c89699dbc9a08dec37d956ae0c2a01bbc
Subhojit Mandal <subhojitmandal912@gmail.com>
2026-06-05 00:11:14 +0530

    day3 refactor

--- expense_trackerV2.py
+++ expense_trackerV2.py
@@ -1,85 +1,86 @@
 print("Expense Tracker")
 
+expenses = []
+
+
+def load_expenses():
+
+    keys_list = ["name", "amount", "category"]
+
+    with open('expenses.txt', 'r') as r:
+        for line in r:
+            clean_line = line.strip()
+            new_dict = dict(zip(keys_list, clean_line.split(",")))
+            new_dict['amount'] = int(new_dict['amount'])
+            expenses.append(new_dict)
+
+def save_expenses(expense, amount, category):
+    with open('expenses.txt', 'a') as f:
+        f.write(f'{expense},{amount},{category}\n')
+
+    save_dict = {"name" : expense, "amount" : amount, "category" : category}
+    expenses.append(save_dict)
+        
+def add_expense():
+    expense = input("Enter expense name: ").lower()
+
+    while True:
+        try:
+            amount = int(input("Enter amount spent: "))
+        except ValueError:
+            print("Error! Enter correct amount")
+            continue
+        break
+
+    category = input("Enter category: ")
+    save_expenses(expense, amount, category)
+
+
+def show_expenses():
+    if expenses:
+        for x in expenses:
+            print(f'{x['name'].title()} - {x['amount']} - {x['category'].title()}')
+    else:
+        print("No expenses found.")
+
+def calculate_total():
+    totals = {}
+    for i in expenses:
+        category = i['category']
+        if category in totals:
+            totals[category] += i['amount']
+        else:
+            totals[category] = i['amount']
+
+    for expense in totals:
+        amount = totals[expense]
+        print(f'{expense.title()} total = {amount}')
+            
+
+load_expenses()
 
 while True:
 
-    expenses = []
-    food_total =[]
-    travel_total =[]
-    clothes_total =[]
-    food_amount = 0
-    travel_amount = 0
-    clothes_amount = 0
 
     menu = ["\n1. Add Expense", "2. Show Expense", "3. Total Spent", "4. Exit"]
     for i in menu:
         print(i)
 
-    value = input("Enter the index: ")
+    choice = input("Enter the index: ")
     print()
 
-
-    if value in ["1", "2", "3", "4"]:
-
-        keys_list = ["name", "amount", "category"]
-        values_list = []
-
-        with open('expenses.txt', 'r') as r:
-            for line in r:
-                clean_line = line.strip()
-                values_list.append(clean_line.split(","))
+    if choice == "1":
+        add_expense()            
             
-            for i in values_list:
-                    new_dict = dict(zip(keys_list, i))
-                    expenses.append(new_dict)
+    elif choice == "2":
+        show_expenses()
 
-        if value == "1":
-            expense = input("Enter expense name: ")
-            amount = input("Enter amount spent: ")
-            category = input("Enter category: ")
-
-            with open('expenses.txt', 'a') as f:
-                f.write(f'{expense},{amount},{category}\n')
-            
-            with open('expenses.txt', 'r') as r:
-                for line in r:
-                    values_list = line.split(",")
-                    values_list.append(values_list)
-                
-            
-        elif value == "2":
-                             
-            print(expenses)
-
-
-        elif value == "3":
-                        
-            for i in expenses:
-                if i['category'] == 'food':
-                    food_total.append(int(i['amount']))
-                if i['category'] == 'travel':
-                    travel_total.append(int(i['amount']))  
-                if i['category'] == 'clothes':
-                    clothes_total.append(int(i['amount']))
-            
-            if len(food_total) > 0:
-                for i in food_total:
-                    food_amount += i
-                print(f'Food total = {food_amount}')
-
-            if len(travel_total) > 0:
-                for i in travel_total:
-                    travel_amount += i
-                print(f'Travel total = {travel_amount}')
-
-            if len(clothes_total) > 0:
-                for i in clothes_total:
-                    clothes_amount += i
-                print(f'Clothes total = {clothes_amount}')
-
-        elif value == "4":
-            print("Thank you for using Expense Tracker")
-            break
+    elif choice == "3":
+        calculate_total()
+                    
+    elif choice == "4":
+        print("Thank you for using Expense Tracker")
+        break
 
 
 
